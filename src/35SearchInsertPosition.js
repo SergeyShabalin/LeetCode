83564727fc24d06aede887b0f601fe4bd8c96fc0
lock_user <obaldevshiy@yandex.ru>
2023-04-26 14:18:55 +0300

    35. search nsert osition changed

--- src/35SearchInsertPosition.js
+++ src/35SearchInsertPosition.js
@@ -3,53 +3,24 @@
 // Если нет, верните индекс туда, где он был бы, если бы он был вставлен по порядку.
 // Вы должны написать алгоритм со  O(log n)сложностью выполнения.
 
-const searchInsert = function (nums, target, arrLength = 0) {
+const searchInsert = function(nums, target, arrLength = 0) {
+    const len = nums.length;
+    const half = Math.floor(len / 2);
+    const sumIndex = arrLength;
 
-    let changedNums = nums
-    let len = nums.length
-    let half = Math.round(len / 2)
-    let firstHalfArr = nums.slice(0, half)
-    let secondHalfArr = nums.slice(half, len)
-    const sumIndex = arrLength
-
-    let finishIndex = 0
-
-    let lastElemFirst = firstHalfArr[firstHalfArr.length - 1]
-    let firstElemSecond = secondHalfArr[0]
-
-    if (target > nums[len - 1]) {
-        console.log('index', nums.indexOf(nums[len - 1]) + 1)
+    if (len === 1) {
+        if (target > nums[0]){
+            return 1 + sumIndex;
+        } else {
+            return sumIndex;
+        }
     }
 
-    if (target <= lastElemFirst) {
-        console.log('искомое число в первой части массива')
-        changedNums = firstHalfArr
-        if (target === lastElemFirst) {
-            console.log('найденное число', lastElemFirst, 'его индекс ')
-            console.log(nums.indexOf(lastElemFirst) + sumIndex)
-            finishIndex = (nums.indexOf(lastElemFirst) + sumIndex)
-        } else {
-            searchInsert(changedNums, target)
-        }
-
-    }
-    if (target >= firstElemSecond) {
-        let indexed = nums.indexOf(target)
-        changedNums = secondHalfArr
-        console.log('искомое число во второй части массива')
-        if (target === firstElemSecond) {
-            console.log('найденное число', firstElemSecond, 'его индекс ')
-            console.log(indexed + sumIndex)
-            finishIndex = indexed + sumIndex
-        } else {
-            searchInsert(changedNums, target, firstHalfArr.length)
-        }
+    if (target <= nums[half - 1]) {
+        return searchInsert(nums.slice(0, half), target, sumIndex);
     } else {
-        console.log('half', half)
-        finishIndex = half
+        return searchInsert(nums.slice(half), target, sumIndex + half);
     }
-
-    console.log('finishIndex', finishIndex)
 };
 
-searchInsert([1, 3, 5, 6], 5)
+searchInsert([1, 3, 5, 6], 3)
